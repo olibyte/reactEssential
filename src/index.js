23089ec3ac187404f95f99d8741c75ba331dbd8d
Oliver <ocben1@gmail.com>
2019-12-03 13:22:53 +1100

    refactor elements using jsx

--- src/index.js
+++ src/index.js
@@ -16,13 +16,10 @@ var style = {
     fontFamily: 'Arial'
 }
 
-const title = React.createElement(
-    'h1',
-    {id: 'title', className: 'header', style: style},
-    'Hello World'
-)
-
 ReactDOM.render(
-    title,
+    <div style={style}>
+        <h1 id="heading-element">Hello World!</h1>
+        <p>We're glad you're here!</p>
+    </div>,
     document.getElementById('root')
 );
