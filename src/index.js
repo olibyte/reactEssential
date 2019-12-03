5321aabfcaf93eb78c40a0353c7e92c8b6156cdb
Oliver <ocben1@gmail.com>
2019-12-04 08:41:20 +1100

    refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+let bookList = [
+    { "title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
+    { "title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
+    { "title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 },
+    { "title": "Hunger", "author": "Ernest Hemingway", "pages": 320 }
+]
+
 const Book = ({ title, author, pages }) => {
     return (
         <section>
@@ -10,18 +17,23 @@ const Book = ({ title, author, pages }) => {
         </section>
     )
 };
-const Library = () => {
+const Library = ({ books }) => {
     return (
         <div>
-            <Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260} />
-            <Book title="White Teeth" author="Zadie Smith" pages={480} />
-            <Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304} />
+            {books.map(
+                (book, i) =>
+                    <Book
+                        key={i}
+                        title={book.title}
+                        author={book.author}
+                        pages={book.pages} />
+            )}
         </div>
     )
 };
 
 render(
-    <Library />
+    <Library books={bookList} />
     ,
     document.getElementById('root')
 );
