61137aa92eaf67eb9a6b005bad548f3b58738381
Oliver <ocben1@gmail.com>
2019-12-03 22:01:33 +1100

    add custom methods

--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+let skiData = {
+    total: 50,
+    powder: 20,
+    backcountry: 10,
+    goal: 100
+}
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-class Message extends React.Component {
+class SkiDayCounter extends Component {
+    getPercent = decimal => {
+        return decimal * 100 + '%';
+    }
+    calcGoalProgress = (total, goal) => {
+        return this.getPercent(total/goal);
+    }
     render() {
+        const {total, powder, backcountry, goal} = this.props;
         return (
-            <div>
-                <h1 style={{color: this.props.color}}>{this.props.msg}</h1>
-                <p>I'll check back in {this.props.minutes} minutes</p>
-            </div>
+            <section>
+                <div>
+                    <p>Total Days: {total}</p>
+                </div>
+                <div>
+                    <p>Powder Days: {powder}</p>
+                </div>
+                <div>
+                    <p>Backcountry Days: {backcountry}</p>
+                </div>
+                <div>
+                    <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+                </div>
+            </section>
         )
     }
 }
 
-ReactDOM.render(
-    <Message color="blue" msg="hello, how are you?" minutes={50}/>,
+render (
+    <SkiDayCounter
+        total={skiData.total}
+        powder={skiData.powder}
+        backcountry={skiData.backcountry}
+        goal={skiData.goal}
+    />,
     document.getElementById('root')
 );
