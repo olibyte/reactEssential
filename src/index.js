3ce430d7b49e9b5d30717fac72b7a56b21ab8dba
Oliver <ocben1@gmail.com>
2019-12-04 18:28:22 +1100

    setstate

--- src/index.js
+++ src/index.js
@@ -23,13 +23,19 @@ class Library extends Component {
         this.state = {
             open: false
         }
+        this.toggleOpenClosed = this.toggleOpenClosed.bind(this);
+    }
+    toggleOpenClosed() {
+        this.setState(prevState => ({
+            open: !this.state.open
+        }))
     }
     render() {
-        console.log(this.state)
-        const {books} = this.props;
+        const { books } = this.props;
         return (
             <div>
-                <h1>The library is {this.state.open ? 'open' : 'closed' }</h1>
+                <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
                     (book, i) =>
                         <Book
