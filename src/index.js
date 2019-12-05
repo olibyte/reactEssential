fbef1d01f31aca812eb81180cf45cf72089c12ca
Oliver <ocben1@gmail.com>
2019-12-06 08:12:51 +1100

    fetch data

--- src/index.js
+++ src/index.js
@@ -34,15 +34,20 @@ class Library extends Component {
     state = { 
         open: true,
         freeBookmark: true,
-        hiring: false
+        hiring: false,
+        data: [],
+        loading: false
     }
 
     componentDidMount() {
-
+        this.setState({loading: true});
+        fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+        .then(data => data.json())
+        .then(data => this.setState({data, loading: false}))
     }
 
     componentDidUpdate() {
-        
+
     }
     
     toggleOpenClosed = () => {
@@ -55,6 +60,19 @@ class Library extends Component {
         return (
             <div>
                 {this.state.hiring ? <Hiring /> : <NotHiring />}
+                {this.state.loading
+                ? "loading"
+                : <div>
+                        {this.state.data.map(product => {
+                            return (
+                                <div>
+                                    <h3>Library product of the week!</h3>
+                                    <h4>{product.name}</h4>
+                                    <img src={product.image} height={100} />
+                                </div>
+                            )
+                        })}
+                    </div>}
                 <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
                 <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
